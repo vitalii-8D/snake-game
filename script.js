6471bbbcc57ba8c9e4a83e30ac4f77dcab678470
vitalii-8D <gotbubu0@gmail.com>
2020-11-27 21:34:03 +0200

    - changed increasing the snake

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Creating field
+// Create field
 let field = document.createElement('div');
 document.body.appendChild(field);
 field.classList.add('field');
 
-// Creating blocks
+// Create blocks
 for (let i=1; i<101; i++) {
     let excel = document.createElement('div');
     field.appendChild(excel);
@@ -13,6 +13,7 @@ for (let i=1; i<101; i++) {
 let excel = document.getElementsByClassName('excel');
 let x = 1, y = 10;
 
+// Give coordinates to excels
 for (let i=0; i<excel.length; i++) {
     if (x > 10) {
         x = 1;
@@ -23,8 +24,7 @@ for (let i=0; i<excel.length; i++) {
     x++;
 }
 
-// Snake generation
-
+// Generate snake
 function generateSnake() {
     let posX = Math.round(Math.random() * (10 - 3) + 3);
     let posY = Math.round(Math.random() * (10 - 1) + 1);
@@ -33,11 +33,10 @@ function generateSnake() {
 }
 let coordinates = generateSnake();
 
+// Create initial snake
 let snakeBody = [document.querySelector('[posX = "' + coordinates[0] + '"][posY = "' + coordinates[1] + '"]'),
     document.querySelector('[posX = "' + (+coordinates[0] - 1) + '"][posY = "' + coordinates[1] + '"]'),
     document.querySelector('[posX = "' + (+coordinates[0] - 2) + '"][posY = "' + coordinates[1] + '"]')]
-
-console.log(snakeBody);
 
 for (let i=0; i<snakeBody.length; i++) {
     snakeBody[i].classList.add('snakeBody');
@@ -45,8 +44,7 @@ for (let i=0; i<snakeBody.length; i++) {
 snakeBody[0].classList.add('head');
 
 
-// Creating of mouse
-
+// Mouse(tomato) creating
 let mouse;
 
 function createMouse() {
@@ -58,7 +56,6 @@ function createMouse() {
     }
 
     let mouseCoordinates = generateMouse();
-    console.log(mouseCoordinates);
 
     mouse = document.querySelector('[posX = "' + mouseCoordinates[0] + '"][posY = "' + mouseCoordinates[1] + '"]')
 
@@ -74,8 +71,7 @@ function createMouse() {
 createMouse();
 
 
-// Moves
-
+// Move
 let direction = 'right';
 let steps = false;
 
@@ -93,6 +89,7 @@ function move() {
     snakeBody[snakeBody.length - 1].classList.remove('snakeBody');
     snakeBody.pop();
 
+    // Moving according the direction
     if (direction === 'right') {
         if (snakeCoordinates[0] < 10) {
             let newBodyPart = document.querySelector('[posX = "' + (+snakeCoordinates[0] + 1) + '"][posY = "' + snakeCoordinates[1] + '"]');
@@ -127,18 +124,28 @@ function move() {
         }
     }
 
+    // When the snake catch the mouse(tomato)
     if (snakeBody[0].getAttribute('posX') === mouse.getAttribute('posX')
     && snakeBody[0].getAttribute('posY') === mouse.getAttribute('posY')) {
         mouse.classList.remove('mouse');
-        let a = snakeBody[snakeBody.length - 1].getAttribute('posX');
-        let b = snakeBody[snakeBody.length - 1].getAttribute('posY');
-        snakeBody.push(document.querySelector('[posX = "' + a + '"][posY = "' + b + '"]'));
+        /*let a = snakeBody[snakeBody.length - 1].getAttribute('posX');
+        let b = snakeBody[snakeBody.length - 1].getAttribute('posY');*/
+        let a = mouse.getAttribute('posX');
+        let b = mouse.getAttribute('posY');
+        // Increase the snake when it take a tomato
+        // snakeBody.push(document.querySelector('[posX = "' + a + '"][posY = "' + b + '"]'));
+        // Increase the snake when it goes through the tomato 
+        snakeBody.unshift(document.querySelector('[posX = "' + a + '"][posY = "' + b + '"]'));
         createMouse();
+
+        console.log('after eating');
+        console.log(snakeBody);
 
         score++;
         input.value = `Ваши очки: ${score}`;
     }
 
+    // When the snake eats itself
     if (snakeBody[0].classList.contains('snakeBody')) {
         clearInterval(interval);
         setTimeout(() => {
